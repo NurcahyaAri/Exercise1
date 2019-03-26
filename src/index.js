a15d5469dc74ab849e1eb82818630fd7110fc74a
Ari Nurcahya <nurcahyaari@gmail.com>
2019-03-26 21:20:42 +0700

    exercise update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import "./styles.css";
-import "bulma";
+import "bootstrap";
+import "jquery";
+import "popper.js";
 // document.getElementById("app").innerHTML = `
-// <h1>Hello Vanilla!</h1>
-// <div>
-//   We use Parcel to bundle this sandbox, you can find more info about Parcel
-//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// <div class="container">
+//   <div class="card" style="width: 18rem;">
+//     <div class="card-body">
+//       <h5 class="card-title">Card title</h5>
+//       <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     </div>
+//   </div>
 // </div>
 // `;
